8921ea1844235c6ab4867b80d37c66b84da75a98
Shivnath Kumar <108025503+Singhaniya4u@users.noreply.github.com>
2025-03-27 14:59:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function calculate() {
 
 // Handle keyboard input
 document.addEventListener('keydown', function(event) {
+    alert("Calculate");
     const allowedKeys = '0123456789+-*/.';
     if (allowedKeys.includes(event.key) || event.key === 'Enter' || event.key === 'Backspace') {
         if (event.key === 'Enter') {
